1e632a03244ec494cd6e95a86747a5ccd2466b4a
Minh Le <duyminh.le310@gmail.com>
2022-06-23 13:44:09 -0400

    125. Valid Palindrome

--- src/125.ValidPalindrome.js
+++ src/125.ValidPalindrome.js
@@ -2,17 +2,22 @@
  * @param {string} s
  * @return {boolean}
  */
-
-const isAlphabet = (str) => /[a-zA-Z0-9]/.test(str);
-
 var isPalindrome = function (s) {
   const sArr = s
     .toLowerCase()
     .split("")
-    .filter((c) => isAlphabet(c));
+    .filter((c) => /[A-Za-z0-9]/.test(c));
 
-  for (let i = 0; i < sArr.length / 2; i++) {
-    if (sArr[i] !== sArr[sArr.length - i - 1]) return false;
+  let start = 0,
+    end = sArr.length - 1;
+
+  while (start < end) {
+    if (sArr[start] !== sArr[end]) {
+      return false;
+    }
+
+    start++;
+    end--;
   }
 
   return true;
