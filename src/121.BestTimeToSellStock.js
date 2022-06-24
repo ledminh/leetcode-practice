71dc0c1c06e9f09998c436dc84259603dd109de3
Minh Le <duyminh.le310@gmail.com>
2022-06-24 14:25:30 -0400

    121. Best Time to Buy and Sell Stock

--- src/121.BestTimeToSellStock.js
+++ src/121.BestTimeToSellStock.js
@@ -1,15 +1,47 @@
+// /**
+//  * @param {number[]} prices
+//  * @return {number}
+//  */
+// var maxProfit = function (prices) {
+//   let minPrice = prices[0],
+//     maxProfit = prices[0] - minPrice;
+
+//   for (let i = 1; i < prices.length; i++) {
+//     if (prices[i] < minPrice) minPrice = prices[i];
+
+//     if (prices[i] - minPrice > maxProfit) maxProfit = prices[i] - minPrice;
+//   }
+
+//   return maxProfit;
+// };
+
+/************************
+ * SECOND TRY
+ */
+
 /**
  * @param {number[]} prices
  * @return {number}
  */
 var maxProfit = function (prices) {
-  let minPrice = prices[0],
-    maxProfit = prices[0] - minPrice;
+  if (prices.length < 2) {
+    return 0;
+  }
 
-  for (let i = 1; i < prices.length; i++) {
-    if (prices[i] < minPrice) minPrice = prices[i];
+  let buyI = 0,
+    sellI = 0,
+    maxProfit = 0;
 
-    if (prices[i] - minPrice > maxProfit) maxProfit = prices[i] - minPrice;
+  while (sellI < prices.length) {
+    const profit = prices[sellI] - prices[buyI];
+
+    if (profit < 0) {
+      buyI = sellI;
+    } else if (profit > maxProfit) {
+      maxProfit = profit;
+    }
+
+    sellI++;
   }
 
   return maxProfit;
