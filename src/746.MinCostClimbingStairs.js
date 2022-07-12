1000280d4fcdd0088b3ec43623d1c967460bd483
Minh Le <duyminh.le310@gmail.com>
2022-07-12 19:17:30 -0400

    746. Min Cost Climbing Stairs

---
+++ src/746.MinCostClimbingStairs.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} cost
+ * @return {number}
+ */
+var minCostClimbingStairs = function (cost) {
+  for (let i = cost.length - 3; i >= 0; i--) {
+    cost[i] = Math.min(cost[i] + cost[i + 1], cost[i] + cost[i + 2]);
+  }
+
+  return Math.min(cost[0], cost[1]);
+};
